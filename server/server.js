97bc89b3dd784af95242d264b179e4c1a5e0b125
bhawesh2020 <bhawesh2020@gmail.com>
2018-11-11 13:38:58 +0530

    Setup separate database for test and production

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _=require('lodash');
 const express=require('express');
 const bodyParser=require('body-parser');
@@ -9,7 +11,7 @@ var {todo}=require('./models/todos.js');
 var {user}=require('./models/user.js');
 
 var app=express();
-var port=process.env.PORT || 3000;
+var port=process.env.PORT;
 
 app.use(bodyParser.json());
 
