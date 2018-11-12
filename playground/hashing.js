915862e827f1c7e055cac9467d6e50092b357a50
bhawesh2020 <bhawesh2020@gmail.com>
2018-11-13 00:05:50 +0530

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
-    id:5
-};
+var password='qwerty12345';
 
-var token=jwt.sign(data,'123qwerty');
-console.log(token);
-var decoded=jwt.verify(token,'123qwerty');
-console.log(decoded);
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+    });
+});
+
+var hashedPass='$2a$10$1fjtoU8GM1NVqj6HiEecVe3HWtNkuO1HVtO8G5pYct0KHne8Tc8he';
+bcrypt.compare(password,hashedPass,(err,res)=>{
+    console.log(res);
+});
+
+
+// var data={
+//     id:5
+// };
+
+// var token=jwt.sign(data,'123qwerty');
+// console.log(token);
+// var decoded=jwt.verify(token,'123qwerty');
+// console.log(decoded);
 
 // var msg="Hello world";
 // var hash=SHA256(msg).toString();
