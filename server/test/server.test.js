7b402a74456ebbf03267c8f4e182581dfec74c25
bhawesh2020 <bhawesh2020@gmail.com>
2018-11-16 12:22:16 +0530

    Add test for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -327,3 +327,23 @@ describe('POST /users/login',()=>{
         });
     });
 });
+
+describe('DELETE /users/me/token',()=>{
+    it('should remove auth token on logout',(done)=>{
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth',users[0].tokens[0].token)
+            .expect(200)
+        .end((err,res)=>{
+            if(err)
+            {
+                return done(err);
+            }
+            user.findById(users[0]._id).then((result)=>{
+                expect(result.tokens.length).toBe(0);
+                done();
+            }).catch((e)=>done(e));
+        });
+    });
+    
+});
